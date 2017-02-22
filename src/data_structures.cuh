1d87664e68da65716e91ea90cc53dfae5a6b5344
R012 <mario.cobos@edu.uah.es>
2017-02-22 17:43:10 +0100

    Added basic data structures definition
    May need some fine tuning later down the line. Also included a kernel
    source file to use as main.

---
+++ src/data_structures.cuh
@@ -0,0 +1,132 @@
+#ifndef GAME_DATA_STRUCTS
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+	EN: Enum defining number-jewel color associations.
+	ES: Enum que define las asociaciones entre número y color de joya.
+*/
+enum jewels{BLUE = 1, RED = 2, ORANGE = 3, GREEN = 4, BROWN = 5, YELLOW = 6, BLACK = 7, WHITE = 8};
+/*
+	EN: Table struct. It contains information regarding current game status, as well as a variety of functions to help handle it.
+	ES: Struct de Table. Contiene información referente al estado actual del juego, además de múltiples funciones para ayudar a manejarlo.
+*/
+typedef struct{
+	int width, height, stride;
+	int* elements;
+	
+	/*
+		EN: Sets the value of the specified element.
+		ES: Establece el valor del elemento especificado.
+	*/
+	void setElement(int value, int row, int col)
+	{
+		elements[row*stride+col] = value;
+	}
+	
+	/*
+		EN: Retrieves the value of the specified element.
+		ES: Recupera el valor del elemento especificado.
+	*/
+	int getElement(int row, int col)
+	{
+		return elements[row*stride+col];
+	}
+	
+	/*
+		EN: Creates a random table status; used when initializing.
+		ES: Crea un estado de tablero aleatorio. Usado cuando se inicializa.
+	*/
+	void randomize()
+	{
+		for(int i=0; i<height; i++)
+		{
+			for(int j=0; j<width; j++)
+			{
+				setElement(1+rand()%7, i, j);
+			}
+		}
+	}
+	
+	/*
+		EN: Creates a new empty table with the established width and height.
+		ES: Crea un nuevo tablero con la anchura y altura establecidos.
+	*/
+	void initialize(int tWidth, int tHeight)
+	{
+		width = tWidth;
+		height = tHeight;
+		stride = width;
+		elements = (int*)calloc(width*height, sizeof(int));
+		randomize();
+	}
+	
+	/*
+		EN: Allocates memory for the table in the target device.
+		ES: Asigna memoria al tablero en el dispositivo.
+	*/
+	void deviceInitialize(int tWidth, int tHeight)
+	{
+		width = tWidth;
+		height = tHeight;
+		stride = width;
+		cudaMalloc(elements, width*height*sizeof(int));
+	}
+	
+	/*
+		EN: Prints the contents of the table to stdout.
+		ES: Dibuja el contenido del tablero en el output standard.
+	*/
+	void print()
+	{
+		for(int i=0; i<height; i++)
+			{
+				for(int j=0; j<width; j++)
+					printf("%i\t", elements[i*stride+j]);
+				printf("\n");
+			}
+	}
+	
+	/*
+		EN: Retrieves a given subtable of region of the table.
+		ES: Recupera un subtablero o region dada del tablero. 
+	*/
+	Table getSubTable(int startRow, int startCol, int sWidth, int sHeight)
+	{
+		Table newTable;
+		newTable.initialize(sWidth, sHeight);
+		free(newTable.elements);
+		newTable.elements = &(elements[startRow*stride+startCol]);
+		return newTable;
+	}
+	
+	/*
+		EN: Recreates a given region of the table.
+		ES: Recrea una región dada del tablero.
+	*/
+	void recreateRegion(int startRow, int startCol, int rWidth, int rHeight)
+	{
+		Table region = getSubtable(startRow, startCol, rWidth, rHeight);
+		region.randomize();
+	}
+	
+	/*
+		EN: Frees the memory used by the pointers in the table.
+		ES: Libera la memoria utilizada por los punteros del tablero.
+	*/
+	void destroy()
+	{
+		free(elements);
+	}
+	
+	/*
+		EN: Frees the memory used in the device by the table.
+		ES: Libera la memoria utilizada por los punteros del tablero en el dispositivo.
+	*/
+	void deviceDestroy()
+	{
+		cudaFree(elements);
+	}
+} Table;
+
+#endif
---
+++ src/kernel.cu
@@ -0,0 +1,6 @@
+#include "data_structures.cuh"
+
+int main(int argc, char* args[])
+{
+	return 0;
+}
